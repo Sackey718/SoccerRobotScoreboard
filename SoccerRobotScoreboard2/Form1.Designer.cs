334d6f7a30479263692800b7affdd46fa741c978
Sackey718 <yuuki718@gmail.com>
2015-10-19 23:34:43 +0900

    UIと終了時の処理を追加

--- SoccerRobotScoreboard2/Form1.Designer.cs
+++ SoccerRobotScoreboard2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SoccerRobotScoreboard2
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 必要なデザイナー変数です。
@@ -29,11 +29,145 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.labelRed = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItemMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemSetting = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelBlue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelRed
+            // 
+            this.labelRed.BackColor = System.Drawing.SystemColors.Control;
+            this.labelRed.Font = new System.Drawing.Font("MS UI Gothic", 700F);
+            this.labelRed.ForeColor = System.Drawing.Color.Red;
+            this.labelRed.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelRed.Location = new System.Drawing.Point(0, 69);
+            this.labelRed.Margin = new System.Windows.Forms.Padding(0);
+            this.labelRed.Name = "labelRed";
+            this.labelRed.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.labelRed.Size = new System.Drawing.Size(770, 934);
+            this.labelRed.TabIndex = 0;
+            this.labelRed.Text = "0";
+            this.labelRed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemMenu,
+            this.ToolStripMenuItemExit});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1920, 26);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ToolStripMenuItemMenu
+            // 
+            this.ToolStripMenuItemMenu.BackColor = System.Drawing.SystemColors.Control;
+            this.ToolStripMenuItemMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemSetting});
+            this.ToolStripMenuItemMenu.Name = "ToolStripMenuItemMenu";
+            this.ToolStripMenuItemMenu.Size = new System.Drawing.Size(68, 22);
+            this.ToolStripMenuItemMenu.Text = "メニュー";
+            // 
+            // ToolStripMenuItemSetting
+            // 
+            this.ToolStripMenuItemSetting.BackColor = System.Drawing.SystemColors.Control;
+            this.ToolStripMenuItemSetting.Name = "ToolStripMenuItemSetting";
+            this.ToolStripMenuItemSetting.Size = new System.Drawing.Size(100, 22);
+            this.ToolStripMenuItemSetting.Text = "設定";
+            this.ToolStripMenuItemSetting.Click += new System.EventHandler(this.ToolStripMenuItemSetting_Click);
+            // 
+            // ToolStripMenuItemExit
+            // 
+            this.ToolStripMenuItemExit.Name = "ToolStripMenuItemExit";
+            this.ToolStripMenuItemExit.Size = new System.Drawing.Size(44, 22);
+            this.ToolStripMenuItemExit.Text = "終了";
+            this.ToolStripMenuItemExit.Click += new System.EventHandler(this.ToolStripMenuItemExit_Click);
+            // 
+            // labelBlue
+            // 
+            this.labelBlue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelBlue.BackColor = System.Drawing.SystemColors.Control;
+            this.labelBlue.Font = new System.Drawing.Font("MS UI Gothic", 700F);
+            this.labelBlue.ForeColor = System.Drawing.Color.Blue;
+            this.labelBlue.Location = new System.Drawing.Point(1150, 69);
+            this.labelBlue.Name = "labelBlue";
+            this.labelBlue.Size = new System.Drawing.Size(770, 934);
+            this.labelBlue.TabIndex = 0;
+            this.labelBlue.Text = "0";
+            this.labelBlue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 500F);
+            this.label1.Location = new System.Drawing.Point(654, 225);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(613, 667);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "-";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 150F);
+            this.label2.Location = new System.Drawing.Point(0, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1920, 199);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "サッカーロボット";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelBlue);
+            this.Controls.Add(this.labelRed);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "サッカーロボット得点板";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormMain_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelRed;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemMenu;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemSetting;
+        private System.Windows.Forms.Label labelBlue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemExit;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- SoccerRobotScoreboard2/Form1.cs
+++ SoccerRobotScoreboard2/Form1.cs
@@ -7,14 +7,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace SoccerRobotScoreboard2
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        private SerialPort BT = null;
+        int RedPoint = 0;
+        int BluePoint = 0;
+
+        public FormMain()
         {
             InitializeComponent();
         }
+
+        private void DispPoint()
+        {
+            labelRed.Text = RedPoint.ToString();
+            labelBlue.Text = BluePoint.ToString();
+        }
+
+        private void Exit()
+        {
+            if (BT != null)
+            {
+                BT.Close();
+            }
+            this.Close();
+        }
+
+        private void ToolStripMenuItemSetting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripMenuItemExit_Click(object sender, EventArgs e)
+        {
+            Exit();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void FormMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Escape)
+            {
+                Exit();
+            }
+
+        }
+
     }
 }
